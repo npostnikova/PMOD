04c805e58bed279d75f1192914753976135925af
nastya <postnikovaanastasiaa@gmail.com>
2020-04-14 08:59:23 +0300

    Add deacrease key specialization.

--- Galois-2.2.1/include/Galois/WorkList/AdaptiveMultiQueue.h
+++ Galois-2.2.1/include/Galois/WorkList/AdaptiveMultiQueue.h
@@ -4,114 +4,119 @@
 #include <atomic>
 #include <memory>
 #include <cstdlib>
+#include <mutex>
+#include <ctime>
+#include <random>
+#include "Heap.h"
 
 namespace Galois {
 namespace WorkList {
 
-template<class Comparer, typename T, size_t C = 2, bool Concurrent = true>
+/**
+ * Lockable heap structure.
+ *
+ * @tparam T type of stored elements
+ * @tparam Comparer callable defining ordering for objects of type `T`.
+ * Its `operator()` returns `true` iff the first argument should follow the second one.
+ */
+template <typename  T, typename Comparer>
+struct LockableHeap {
+	DAryHeap<T, Comparer, 8> heap;
+	// todo: use atomic
+	T min;
+
+	//! Non-blocking lock.
+	inline bool try_lock() {
+		bool expected = false;
+		return _lock.compare_exchange_strong(expected, true);
+	}
+
+	//! Unlocks the queue.
+	inline void unlock() {
+		_lock = false;
+	}
+
+private:
+	std::atomic<bool> _lock;
+};
+
+/**
+ * Basic implementation. Provides effective pushing of range of elements only.
+ *
+ * @tparam T type of elements
+ * @tparam Comparer comparator for elements of type `T`
+ * @tparam DecreaseKey if auto decrease key should be supported
+ * @tparam DecreaseKeyIndexer indexer for decrease key operation
+ * @tparam C parameter for queues number
+ * @tparam Concurrent if the implemention should be concurrent
+ */
+template<typename T,
+				 typename Comparer,
+				 bool DecreaseKey = false,
+				 typename DecreaseKeyIndexer = void,
+				 size_t C = 2,
+				 bool Concurrent = true>
 class AdaptiveMultiQueue {
 private:
-  typedef boost::heap::d_ary_heap<T,
-              boost::heap::arity<8>,
-              boost::heap::compare<Comparer>
-              > DAryHeap;
-
-  //! Lockable heap structure.
-  struct Heap {
-    DAryHeap heap;
-    T min;
-
-    //! Initialize Heap class. Must be called before the class is used.
-    static void init() {
-      memset(reinterpret_cast<void *>(&maxT), 0xff, sizeof(maxT));
-
-    }
-
-    //! Initialize an instance.
-    void set_max_val() {
-      // todo move it to constructor or fix AMQ allocation
-      min = maxT;
-      heap.emplace(min);
-    }
-
-    //! Blocking lock.
-    inline void lock() {
-      bool expected = false;
-      while (!_lock.compare_exchange_strong(expected, true)) {}
-    }
-
-    //! Non-blocking lock.
-    inline bool try_lock() {
-      bool expected = false;
-      return _lock.compare_exchange_strong(expected, true);
-    }
-
-    //! Unlocks the queue.
-    inline void unlock() {
-      _lock = false;
-    }
-
-    //! Returns maximum value of T.
-    T& max_val() {
-      return maxT;
-    }
-
-  private:
-    inline static T maxT;
-    std::atomic<bool> _lock;
-  };
-
+	typedef LockableHeap<T, Comparer> Heap;
   std::unique_ptr<Runtime::LL::CacheLineStorage<Heap>[]> heaps;
   Comparer compare;
+  //! Number of queues.
   const size_t nQ;
+  //! Maximum element of type `T`.
+	T maxT;
 
-  void init_heaps() {
-    Heap::init();
-    for (size_t i = 0; i < nQ; i++) {
-      heaps[i].data.set_max_val();
-    }
-  }
+  //! Thread local random.
+	size_t rand_heap() {
+		static thread_local std::mt19937 generator;
+		static thread_local std::uniform_int_distribution<size_t> distribution(0, nQ - 1);
+		return distribution(generator);
+	}
 
 public:
   AdaptiveMultiQueue() : nQ(Galois::getActiveThreads() * C) {
-  //heaps(new Runtime::LL::CacheLineStorage<Heap>[nQ]) {
-    // TODO make_unique + why does it throw bad_alloc /\??
-    heaps = std::unique_ptr<Runtime::LL::CacheLineStorage<Heap>[]>(new Runtime::LL::CacheLineStorage<Heap>[nQ]);
-    init_heaps();
-    std::srand(std::time(0));
+    heaps = std::make_unique<Runtime::LL::CacheLineStorage<Heap>[]>(nQ);
+
+	  memset(reinterpret_cast<void *>(&maxT), 0xff, sizeof(maxT));
+    for (int i = 0; i < nQ; i++) {
+	  	heaps[i].data.heap.set_index(i);
+	  	heaps[i].data.heap.set_max_val(maxT);
+		  heaps[i].data.min = maxT;
+		  heaps[i].data.heap.push(heaps[i].data.min);
+	  }
   }
 
-  //! T is the value type of the WL
+  //! T is the value type of the WL.
   typedef T value_type;
 
-  //! change the concurrency flag
+  //! Change the concurrency flag.
   template<bool _concurrent>
   struct rethread {
-    typedef AdaptiveMultiQueue<Comparer, T, C, _concurrent> type;
+    typedef AdaptiveMultiQueue<T, Comparer, DecreaseKey, DecreaseKeyIndexer, C, _concurrent> type;
   };
 
-  //! change the type the worklist holds
+  //! Change the type the worklist holds.
   template<typename _T>
   struct retype {
-    typedef AdaptiveMultiQueue<Comparer, _T, C, Concurrent> type;
+    typedef AdaptiveMultiQueue<_T, Comparer, DecreaseKey, DecreaseKeyIndexer, C, Concurrent> type;
   };
 
-  //! push a value onto the queue
+  //! Push a value onto the queue.
   void push(const value_type &val) {
     Heap* heap;
     int q_ind;
 
     do {
-      q_ind = std::rand() % nQ;
+      q_ind = rand_heap();
       heap = &heaps[q_ind].data;
     } while (!heap->try_lock());
 
-    heap->heap.emplace(val);
-    heap->min = heap->heap.top();
+    heap->heap.push(val);
+    heap->min = heap->heap.min();
     heap->unlock();
   }
 
-  //! push a range onto the queue
+  //! Push a range onto the queue.
   template<typename Iter>
   unsigned int push(Iter b, Iter e) {
     const size_t chunk_size = 64;
@@ -123,39 +128,38 @@ public:
       int q_ind;
 
       do {
-        q_ind = std::rand() % nQ;
+        q_ind = rand_heap();
         heap = &heaps[q_ind].data;
       } while (!heap->try_lock());
 
       for (size_t cnt = 0; cnt < chunk_size && b != e; cnt++, npush++) {
-        heap->heap.emplace(*b++);
+      	heap->heap.push(*b++);
       }
-      heap->min = heap->heap.top();
+      heap->min = heap->heap.min();
       heap->unlock();
     }
     return npush;
   }
 
-  //! push initial range onto the queue
-  //! called with the same b and e on each thread
+  //! Push initial range onto the queue.
+  //! Called with the same b and e on each thread.
   template<typename RangeTy>
   unsigned int push_initial(const RangeTy &range) {
     auto rp = range.local_pair();
     return push(rp.first, rp.second);
   }
 
-  //! pop a value from the queue.
+  //! Pop a value from the queue.
   Galois::optional<value_type> pop() {
     Galois::optional<value_type> result;
-
     Heap* heap_i, * heap_j;
     int i_ind, j_ind;
 
     do {
-      i_ind = std::rand() % nQ;
+      i_ind = rand_heap();
       heap_i = &heaps[i_ind].data;
 
-      j_ind = std::rand() % nQ;
+      j_ind = rand_heap();
       heap_j = &heaps[j_ind].data;
 
       if (i_ind == j_ind)
@@ -167,10 +171,10 @@ public:
 
     if (heap_i->heap.size() == 1) {
       heap_i->unlock();
-      for (j_ind = 1; j_ind < nQ; j_ind++) {
-        heap_i = &heaps[(i_ind + j_ind) % nQ].data;
-        if (heap_i->min == heap_i->max_val()) continue;
-        heap_i->lock();
+      for (size_t k = 1; k < nQ; k++) {
+        heap_i = &heaps[(i_ind + k) % nQ].data;
+        if (heap_i->min == maxT) continue;
+        if (!heap_i->try_lock()) continue;
         if (heap_i->heap.size() > 1)
           goto deq;
         heap_i->unlock();
@@ -180,13 +184,199 @@ public:
     }
 
     deq:
-    result = heap_i->heap.top();
-    heap_i->heap.pop();
-    heap_i->min = heap_i->heap.top();
-    heap_i->unlock();
-    return result;
+    result = heap_i->heap.min();
+    heap_i->heap.extractMin();
+    heap_i->min = heap_i->heap.min();
+	  heap_i->unlock();
+	  return result;
   }
+};
 
+/**
+ * Specialization with decrease key support.
+ *
+ * @tparam Indexer indexer for decrease key operation.
+ * Should implement `AbstractDecreaseKeyIndexer` interface.
+ */
+template<typename T,
+	       typename Comparer,
+			   typename Indexer,
+			   size_t C,
+			   bool Concurrent>
+class AdaptiveMultiQueue<T, Comparer, true, Indexer, C, Concurrent> {
+private:
+	typedef LockableHeap<T, Comparer> Heap;
+	std::unique_ptr<Runtime::LL::CacheLineStorage<Heap>[]> heaps;
+	Comparer compare;
+	const size_t nQ;
+	static const bool DecreaseKey = true;
+	Indexer indexer;
+	T maxT;
+
+	//! Thread local random.
+	size_t rand_heap() {
+		static thread_local std::mt19937 generator;
+		static thread_local std::uniform_int_distribution<size_t> distribution(0, nQ - 1);
+		return distribution(generator);
+	}
+
+public:
+	AdaptiveMultiQueue() : nQ(Galois::getActiveThreads() * C) {
+		heaps = std::make_unique<Runtime::LL::CacheLineStorage<Heap>[]>(nQ);
+
+		memset(reinterpret_cast<void *>(&maxT), 0xff, sizeof(maxT));
+		for (int i = 0; i < nQ; i++) {
+			heaps[i].data.heap.set_index(i);
+			heaps[i].data.heap.set_max_val(maxT);
+			heaps[i].data.min = maxT;
+			heaps[i].data.heap.push(heaps[i].data.min);
+		}
+	}
+
+	//! T is the value type of the WL.
+	typedef T value_type;
+
+	//! Change the concurrency flag.
+	template<bool _concurrent>
+	struct rethread {
+		typedef AdaptiveMultiQueue<T, Comparer, DecreaseKey, Indexer, C, _concurrent> type;
+	};
+
+	//! Change the type the worklist holds.
+	template<typename _T>
+	struct retype {
+		typedef AdaptiveMultiQueue<_T, Comparer, DecreaseKey, Indexer, C, Concurrent> type;
+	};
+
+	//! Checks whether the index is a valid index of a queue.
+	inline bool valid_index(int ind) {
+		return ind >= 0 && ind < nQ;
+	}
+
+	//! Update element if the new value is smaller that the value in the heap.
+	//! The element *must* be in the heap.
+	//! Heap *must* be locked.
+	inline void update_elem(const value_type& val, Heap* heap) {
+		heap->heap.decrease_key(indexer, val);
+	}
+
+	//! Add element to the locked heap with index q_ind.
+	//! The element may be in another heap.
+	inline void push_elem(const value_type& val, Heap* heap, size_t q_ind) {
+		Indexer::set_queue(val, -1, q_ind); // fails if the element was added to another queue
+		heap->heap.push(indexer, val);
+	}
+
+	//! Push a value onto the queue.
+	void push(const value_type &val) {
+		Heap* heap;
+		int q_ind;
+
+		do {
+			q_ind = Indexer::get_queue(val);
+			if (!valid_index(q_ind))
+				q_ind = rand_heap();
+			heap = &heaps[q_ind].data;
+		} while (!heap->try_lock());
+
+		if (Indexer::get_queue() == q_ind) {
+			// the element is in the heap
+			update_elem(val);
+		} else {
+			// the element is not in heaps or it has just been added to another heap
+			// todo: check how often the second case happens
+			push_elem(val, heap, q_ind);
+		}
+		heap->min = heap->heap.min();
+		heap->unlock();
+	}
+
+	//! Push a range onto the queue.
+	template<typename Iter>
+	unsigned int push(Iter b, Iter e) {
+		const size_t chunk_size = 64;
+
+		size_t npush = 0;
+
+		while (b != e) {
+			Heap *heap;
+			int q_ind;
+
+			do {
+				q_ind = Indexer::get_queue(*b);
+				if (!valid_index(q_ind))
+					q_ind = rand_heap();
+				heap = &heaps[q_ind].data;
+			} while (!heap->try_lock());
+
+			for (size_t cnt = 0; cnt < chunk_size && b != e; cnt++, npush++) {
+				auto index = Indexer::get_queue(*b);
+				if (index == q_ind) {
+					// the element is in the heap
+					update_elem(*b++, heap);
+				} else if (index == -1) {
+					// no heaps contain the element
+					push_elem(*b++, heap, q_ind);
+				} else {
+					// need to change heap
+					break;
+				}
+			}
+			heap->min = heap->heap.min();
+			heap->unlock();
+		}
+		return npush;
+	}
+
+	//! Push initial range onto the queue.
+	//! Called with the same b and e on each thread.
+	template<typename RangeTy>
+	unsigned int push_initial(const RangeTy &range) {
+		auto rp = range.local_pair();
+		return push(rp.first, rp.second);
+	}
+
+	//! Pop a value from the queue.
+	Galois::optional<value_type> pop() {
+		Galois::optional<value_type> result;
+		Heap* heap_i, * heap_j;
+		int i_ind, j_ind;
+
+		do {
+			i_ind = rand_heap();
+			heap_i = &heaps[i_ind].data;
+
+			j_ind = rand_heap();
+			heap_j = &heaps[j_ind].data;
+
+			if (i_ind == j_ind)
+				continue;
+
+			if (compare(heap_i->min, heap_j->min))
+				heap_i = heap_j;
+		} while (!heap_i->try_lock());
+
+		if (heap_i->heap.size() == 1) {
+			heap_i->unlock();
+			for (size_t k = 1; k < nQ; k++) {
+				heap_i = &heaps[(i_ind + k) % nQ].data;
+				if (heap_i->min == maxT) continue;
+				if (!heap_i->try_lock()) continue;
+				if (heap_i->heap.size() > 1)
+					goto deq;
+				heap_i->unlock();
+			}
+			// empty
+			return result;
+		}
+
+		deq:
+		result = heap_i->heap.min();
+		heap_i->heap.extractMin(indexer);
+		heap_i->min = heap_i->heap.min();
+		heap_i->unlock();
+		return result;
+	}
 };
 
 } // namespace WorkList

a1a8de589fec50325a50ee62554f9267eeb5dfc1
nastya <postnikovaanastasiaa@gmail.com>
2021-05-01 17:52:43 +0300

    [MQ-Optimized] Separate helper-class HeapWithLock

--- Galois-2.2.1/include/Galois/WorkList/MQOptimized/MultiQueueProbProb.h
+++ Galois-2.2.1/include/Galois/WorkList/MQOptimized/MultiQueueProbProb.h
@@ -11,71 +11,9 @@
 #include <thread>
 #include <random>
 #include <iostream>
-#include "../Heap.h"
-#include "../WorkListHelpers.h"
 
 namespace Galois {
 namespace WorkList {
-
-/**
- * Lockable heap structure.
- *
- * @tparam T type of stored elements
- * @tparam Comparer callable defining ordering for objects of type `T`.
- * Its `operator()` returns `true` iff the first argument should follow the second one.
- */
-template <typename  T,
-          typename Comparer,
-          typename Prior = unsigned long,
-          size_t D = 4>
-struct HeapWithLock {
-  DAryHeap<T, Comparer, 4> heap;
-  // Dummy element for setting min when the heap is empty
-  static Prior dummy;
-
-  HeapWithLock() : min(dummy) {}
-
-  //! Non-blocking lock.
-  inline bool try_lock() {
-    return _lock.try_lock();
-  }
-
-  //! Blocking lock.
-  inline void lock() {
-    _lock.lock();
-  }
-
-  //! Unlocks the queue.
-  inline void unlock() {
-    _lock.unlock();
-  }
-
-  Prior getMin() {
-    return min.load(std::memory_order_acquire);
-  }
-
-  static bool isMinDummy(Prior const& value) {
-    return value == dummy;
-  }
-
-  void updateMin() {
-    return min.store(
-      heap.size() > 0 ? heap.min().prior() : dummy,
-      std::memory_order_release
-    );
-  }
-
-private:
-  Runtime::LL::PaddedLock<true> _lock;
-  std::atomic<Prior> min;
-};
-
-template<typename T,
-         typename Compare,
-         typename Prior,
-         size_t D>
-Prior HeapWithLock<T, Compare, Prior, D>::dummy;
-
 
 /**
  * MultiQueue optimized variant. Uses temporal locality idea for

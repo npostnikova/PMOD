7486c1f66db82df061fbc9c0bfc8e0ca9631fdea
nastya <postnikovaanastasiaa@gmail.com>
2021-07-29 01:42:10 +0300

    [Experiments] Add SMQ heatmaps

---
+++ scripts/smq_heatmaps.sh
@@ -0,0 +1,62 @@
+set -e
+
+PROBS=( 1 2 4 8 16 32 64 128 256 512 1024 )
+STEAL_SIZES=( 1 2 4 8 16 32 64 128 256 512 1024 )
+# Supported algorithms
+ALGOS=( "bfs" "sssp" "boruvka" "astar" )
+
+clear_file () {
+  echo "" > $1
+}
+
+generate_heatmaps () {
+  for p in "${PROBS[@]}"
+  do
+    for ss in "${STEAL_SIZES[@]}"
+      do
+        name="smqhm_${p}_${ss}"
+        echo "typedef StealingMultiQueue<UpdateRequest, Comparer, $p, $ss> $name;" >> $1
+        echo "if (wl == \"$name\") RUN_WL($name);" >> $1
+      done
+  done
+}
+
+run_wl_n_times() {
+  for run in $(seq 0 $2); do
+    $MQ_ROOT/scripts/single_run/run_${algo}_${graph}.sh $1 $threads $3
+  done
+}
+
+
+algo=$1
+if ! [[ " ${ALGOS[@]} " =~ " ${algo} " ]]
+then
+  echo "Please, select supported algo: ${ALGOS[@]}"
+  exit 1
+fi
+
+action=$2
+ACTIONS=( build run )
+if ! [[ " ${ACTIONS[@]} " =~ " ${action} " ]]
+then
+  echo "Please, select supported operation: build | run"
+  exit 1
+fi
+
+if [ $action == "build" ]; then
+  file="$GALOIS_HOME/apps/${algo}/Experiments.h"
+  clear_file $file
+  generate_heatmaps $file
+  $MQ_ROOT/scripts/build/build_${algo}.sh
+elif [ $action == "run" ]; then
+  graph=$3
+  threads=$4
+  runs=5
+  for p in "${PROBS[@]}"; do
+    for ss in "${STEAL_SIZES[@]}"; do
+        run_wl_n_times "smqhm_${p}_${ss}" $runs "${algo}_${graph}_smq_$threads"
+    done
+  done
+fi
+
+exit 0

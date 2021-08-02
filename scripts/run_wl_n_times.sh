9a223ba2928c894784111fd9611dd0ddc60b8ad9
nastya <postnikovaanastasiaa@gmail.com>
2021-08-02 06:22:27 +0300

    [Scripts] Fix var in loop

--- scripts/run_wl_n_times.sh
+++ scripts/run_wl_n_times.sh
@@ -7,6 +7,6 @@ result=$([ -z $6 ] && echo "${algo}_${graph}" || echo "${algo}_${graph}_$6")
 
 $MQ_ROOT/scripts/build/build.sh $algo
 
-for in $(seq 0 $times); do
+for i in $(seq 1 $times); do
   $MQ_ROOT/scripts/single_run/run_${algo}_${graph}.sh $wl $threads $result
 done

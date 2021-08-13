10aba4f88e624b3c967696d49b8152bc940b5316
Nikita Koval <ndkoval@ya.ru>
2021-08-13 22:02:59 +0200

    Kill me please

---
+++ PPoPP/AMD/slsmq_heatmaps/numa/cmp_with_usual.sh
@@ -0,0 +1,29 @@
+
+wl=slsmq
+pyscript=~/PMOD/experiments/mq_optimized_hm/find_best.py
+t=256
+for algo in bfs sssp; do
+  for graph in usa west twi web; do
+    echo ">>>>"
+    echo "$algo $graph"
+    python3.7 $pyscript "../${algo}_${graph}_${wl}_$t"
+    python3.7 $pyscript "${algo}_${graph}_${wl}_numa"
+#    python3.7 $pyscript \
+ #     "$MQ_ROOT/experiments/find_params_obim/${algo}_${graph}_obim_$t"
+  #  python3.7 $pyscript \
+   #   "$MQ_ROOT/experiments/find_params_obim/${algo}_${graph}_pmod_$t"
+  done
+done
+
+for algo in boruvka astar; do
+  for graph in usa west; do
+    echo ">>>>"
+    echo "$algo $graph"
+    python3.7 $pyscript "../${algo}_${graph}_${wl}_$t"
+    python3.7 $pyscript "${algo}_${graph}_${wl}_numa"
+  #  python3.7 $pyscript \
+#      "$MQ_ROOT/experiments/find_params_obim/${algo}_${graph}_obim_$t"
+  #  python3.7 $pyscript \
+ #     "$MQ_ROOT/experiments/find_params_obim/${algo}_${graph}_pmod_$t"
+  done
+done

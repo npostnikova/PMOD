31b69a5e485d8c20948357a73319799b2419fb16
Anastasia Postnikova <anastasia.postnikova@app76.spc.labs.intellij.net>
2021-08-13 18:31:36 +0300

    Add NUMA

---
+++ PPoPP/INTEL/mq_heatmaps/problocal/numa/best/run.sh
@@ -0,0 +1,32 @@
+set -e
+
+t=128
+pyscript=$MQ_ROOT/scripts/find_best_wl.py
+for mq in mqpl; do
+  dir=''
+  if [ $mq == "mqpp" ]; then
+    dir="probprob"
+  elif [ $mq == "mqpl" ]; then
+    dir="problocal"
+  elif [ $mq == "mqlp" ]; then
+    dir="localprob"
+  elif [ $mq == "mqll" ]; then
+    dir="locallocal"
+  fi
+  for algo in bfs sssp; do
+    for graph in usa west twi web; do
+        echo ">>>>"
+        echo "$algo $graph"
+        wl_name=$( python3.8 $pyscript "../${algo}_${graph}_${mq}_numa" )
+        $MQ_ROOT/scripts/run_wl_n_times_all_threads.sh $algo $graph $wl_name 5 $mq
+    done
+  done
+  for algo in boruvka astar; do
+    for graph in usa west; do
+          echo ">>>>"
+          echo "$algo $graph"
+          wl_name=$( python3.8 $pyscript "../${algo}_${graph}_${mq}_numa" )
+          $MQ_ROOT/scripts/run_wl_n_times_all_threads.sh $algo $graph $wl_name 5 $mq
+      done
+  done
+done

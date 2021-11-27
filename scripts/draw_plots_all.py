8568dbb072b502a3acbf174d9e1c802139cdce1c
nastya <postnikovaanastasiaa@gmail.com>
2021-11-28 02:10:39 +0300

    [Scripts] Add cmd threads for plots.

--- scripts/draw_plots_all.py
+++ scripts/draw_plots_all.py
@@ -1,4 +1,5 @@
 from plots.draw_plots_helper import draw_plots_for_appendix
+import sys
 
 leg_cols=4
 name ='plots_all'
@@ -12,11 +13,13 @@ timet3=[5, 10, 15, 20, 25, 30, 35,]
 nodet3=[1, 3, 6, 9]
 height=4
 
+threads = sys.argv[1:]
+
 algo_graph = [('sssp', 'twi'), ('sssp', 'web'), ('bfs', 'twi'), ('bfs', 'web')]
 titles = ['SSSP TWITTER', 'SSSP WEB', 'BFS TWITTER', 'BFS WEB']
 
-draw_plots_for_appendix(f'{name}1',
-                        algo_graph, titles, fig_height=height, fig_width=2.7, time_ticks=timet2,
+draw_plots_for_appendix(f'{name}1', algo_graph, titles, threads,
+                        fig_height=height, fig_width=2.7, time_ticks=timet2,
                         colNum=leg_cols, nodes_min=0.5, nodes_max=5, nodes_ticks=nodet2)
 
 
@@ -26,15 +29,15 @@ algo_graph = [('sssp', 'usa'), ('astar', 'usa'), ('bfs', 'usa'), ('bfs', 'west')
 titles = ['SSSP USA', 'A* USA', 'BFS USA', 'BFS WEST']
 
 
-draw_plots_for_appendix(f'{name}2',
-                        algo_graph,titles, fig_height=height, fig_width=2.7, time_ticks=timet1,
-                        colNum=leg_cols, nodes_min=0.5, nodes_max=5, nodes_ticks=nodet1, forbidden_pref=forb, with_legend=False)
+draw_plots_for_appendix(f'{name}2', algo_graph, titles, threads,
+                        fig_height=height, fig_width=2.7, time_ticks=timet1,
+                        colNum=leg_cols, nodes_min=0.5, nodes_max=5, nodes_ticks=nodet1, with_legend=True)
 
 algo_graph = [('sssp', 'west'), ('astar', 'west'), ('boruvka', 'west'), ('boruvka', 'usa')]
 
 titles = ['SSSP WEST', 'A* WEST', 'MST WEST', 'MST USA']
 
-draw_plots_for_appendix(f'{name}3',
-                        algo_graph,titles, fig_height=height, fig_width=2.7, time_ticks=timet3,
-                        colNum=leg_cols, nodes_min=0.5, nodes_max=5, nodes_ticks=nodet3, forbidden_pref=forb, with_legend=False)
+draw_plots_for_appendix(f'{name}3', algo_graph, titles, threads,
+                        fig_height=height, fig_width=2.7, time_ticks=timet3,
+                        colNum=leg_cols, nodes_min=0.5, nodes_max=5, nodes_ticks=nodet3, with_legend=True)
 

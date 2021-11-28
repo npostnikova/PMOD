db3d99df3d24651b2f0d663dc05bf65da5d281ef
nastya <postnikovaanastasiaa@gmail.com>
2021-11-28 15:41:02 +0300

    [Image] Update everything for the image.

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -2,6 +2,30 @@
 
 source $MQ_ROOT/set_envs.sh
 
+################## HEATMAPS ##################
+echo "Computing heatmaps. Be patient!"
+
+hm_dir=$MQ_ROOT/experiments/$CPU/heatmaps
+plt_dir=$MQ_ROOT/experiments/$CPU/plots
+echo "Directory for heatmaps: $hm_dir"
+
+run_hm() {
+  subdir=$1
+  script=$2
+  results_location=$hm_dir/$subdir
+  mkdir -p $results_location
+  cd $results_location
+  echo "Saving heatmaps onto: $results_location"
+  $MQ_ROOT/scripts/$script $OBIM_HM_THREADS $3
+}
+
+echo "Running OBIM heatmaps"
+run_hm obim_heatmaps run_obim_hm.sh
+
+echo "Running PMOD heatmaps"
+run_hm pmod_heatmaps run_pmod_hm.sh
+
+
 ################## PLOTS ##################
 # Running best worklists on different amount of threads (specified in PLT_THREADS).
 
@@ -27,6 +51,12 @@ $MQ_ROOT/scripts/run_other_worklists.sh
 
 ################## DRAWING PICTURES ##################
 pic_dir=$MQ_ROOT/experiments/$CPU/pictures
+
+echo "Drawing heatmaps"
+
+mkdir -p $pic_dir/heatmaps
+cd $pic_dir/heatmaps
+$PYTHON_EXPERIMENTS $MQ_ROOT/scripts/draw_all_heatmaps.py
 
 echo "Drawing plots"
 mkdir -p $pic_dir/plots

cccf31a30a049e0e4278809163ac710e43bbf5f6
nastya <postnikovaanastasiaa@gmail.com>
2021-11-28 14:14:08 +0300

    [Experiments] Update everything for the image.

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
+GALOIS_HOME=./Galois-2.2.1
+
 cd $GALOIS_HOME;
 mkdir build;
 cd build;
@@ -9,10 +11,26 @@ echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/sssp/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/bfs/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/astar/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/boruvka/chunk_size.h
+#echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/pagerank/chunk_size.h
 
 #Create Experiments.h files.
 echo "" > $GALOIS_HOME/apps/sssp/Experiments.h
 echo "" > $GALOIS_HOME/apps/bfs/Experiments.h
 echo "" > $GALOIS_HOME/apps/astar/Experiments.h
 echo "" > $GALOIS_HOME/apps/boruvka/Experiments.h
+#echo "" > $GALOIS_HOME/apps/pagerank/Experiments.h
 
+cd apps/sssp;
+make clean; make;
+
+cd ../bfs;
+make clean; make;
+
+cd ../astar;
+make clean; make
+
+cd ../boruvka;
+make clean; make
+
+cd ../../tools/graph-convert-standalone/
+make clean; make

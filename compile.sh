d53346fd0f16a6c4d8b907c0baf533a786eccb57
nastya <postnikovaanastasiaa@gmail.com>
2021-11-28 17:32:40 +0300

    [Setup] Update compile script.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
-GALOIS_HOME=./Galois-2.2.1
-
 cd $GALOIS_HOME;
 mkdir build;
 cd build;
@@ -11,14 +9,12 @@ echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/sssp/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/bfs/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/astar/chunk_size.h
 echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/boruvka/chunk_size.h
-#echo "#define CHUNK_SIZE 64" > $GALOIS_HOME/apps/pagerank/chunk_size.h
 
 #Create Experiments.h files.
 echo "" > $GALOIS_HOME/apps/sssp/Experiments.h
 echo "" > $GALOIS_HOME/apps/bfs/Experiments.h
 echo "" > $GALOIS_HOME/apps/astar/Experiments.h
 echo "" > $GALOIS_HOME/apps/boruvka/Experiments.h
-#echo "" > $GALOIS_HOME/apps/pagerank/Experiments.h
 
 cd apps/sssp;
 make clean; make;
@@ -31,9 +27,6 @@ make clean; make
 
 cd ../boruvka;
 make clean; make
-#
-#cd ../pagerank;
-#make clean; make
 
 cd ../../tools/graph-convert-standalone/
 make clean; make

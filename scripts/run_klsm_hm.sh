cb39670c4f4a525281f74458b6edfd9ce9dea4ee
nastya <postnikovaanastasiaa@gmail.com>
2021-12-27 21:26:51 +0300

    [Scripts] Set k-LSM min param to 64 as 32 is too slow.

--- scripts/run_klsm_hm.sh
+++ scripts/run_klsm_hm.sh
@@ -5,16 +5,16 @@ source $MQ_ROOT/set_envs.sh
 threads=$1
 
 $MQ_ROOT/scripts/klsm_heatmaps.sh bfs build
-#$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run ctr $threads 32 5000000
-$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run usa $threads 32 5000000
-$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run west $threads 32 5000000
+#$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run ctr $threads 64 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run usa $threads 64 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run west $threads 64 5000000
 #$MQ_ROOT/scripts/klsm_heatmaps.sh bfs run lj $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh bfs run twi $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh bfs run web $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh sssp build
-#$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run ctr $threads 32 5000000
-$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run usa $threads 32 5000000
-$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run west $threads 32 5000000
+#$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run ctr $threads 64 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run usa $threads 64 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run west $threads 64 5000000
 #$MQ_ROOT/scripts/klsm_heatmaps.sh sssp run lj $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh sssp run twi $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh sssp run web $threads 4000000 5000000
@@ -23,6 +23,6 @@ $MQ_ROOT/scripts/klsm_heatmaps.sh boruvka build
 $MQ_ROOT/scripts/klsm_heatmaps.sh boruvka run usa $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh boruvka run west $threads 4000000 5000000
 $MQ_ROOT/scripts/klsm_heatmaps.sh astar build
-$MQ_ROOT/scripts/klsm_heatmaps.sh astar run west $threads 32 5000000
-$MQ_ROOT/scripts/klsm_heatmaps.sh astar run usa $threads 32 5000000
-#$MQ_ROOT/scripts/klsm_heatmaps.sh astar run ctr $threads 32 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh astar run west $threads 64 5000000
+$MQ_ROOT/scripts/klsm_heatmaps.sh astar run usa $threads 64 5000000
+#$MQ_ROOT/scripts/klsm_heatmaps.sh astar run ctr $threads 64 5000000
